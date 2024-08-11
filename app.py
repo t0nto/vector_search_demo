ddba798b3a3ea02ea0e49691190b159fd4254373
t0nto <t.wells.berlin@gmail.com>
2024-08-11 11:52:00 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,147 @@
+import streamlit as st
+import vector_search
+import time
+import plotly.express as px
+import pandas as pd
+import openai
+import json
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if "tool_call" not in st.session_state:
+    st.session_state.tool_call = ""
+
+
+st.title("Vector::Wise")
+st.divider()
+
+
+# Could make function call in openAI more general to specify just x and y vals to return
+def make_bar_chart(x: list[float], y: list[str]) -> None:
+    df = pd.DataFrame({"Vector_Search_Score": x, "Doc_ID": y})
+    fig = px.bar(
+        df,
+        x="Vector_Search_Score",
+        y="Doc_ID",
+        labels={"Vector_Search_Score": "Vector Search Score", "Doc_ID": "Document ID"},
+        title="Vector Search Scores by Document ID",
+    )
+    st.plotly_chart(fig)
+    st.dataframe(df)
+    st.download_button(
+        "Download",
+        df.to_csv(index=False).encode("utf-8"),
+        "vector_search.csv",
+        "text/csv",
+    )
+
+
+if isinstance(
+    st.session_state.tool_call,
+    openai.types.chat.chat_completion_message_tool_call.ChatCompletionMessageToolCall,
+):
+    # st.write("Yay its a tool call!")
+    arguments = json.loads(st.session_state.tool_call.function.arguments)
+    make_bar_chart(arguments["vector_search_score"], arguments["doc_ids"])
+
+# Display chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+placeholder = st.empty()
+
+
+# Add an operator :: so that before the operator is the documents or topic to search, and after is the task to complete
+# Example: "Home decor :: What are the latest trends in home decor?"
+
+
+# Investigate tools and function calls
+# Could be used with mongodb pre filtering, especially for timestamps and dates...
+def query_pipeline() -> None:
+    st.session_state.tool_call = ""
+    with placeholder.status(label="Searching for answers...", expanded=True) as status:
+        status_message = st.empty()
+
+        # query = st.session_state.query
+        if "::" in st.session_state.query:
+            topic, query = [
+                word.strip() for word in st.session_state.query.split("::", 1)
+            ]
+        else:
+            topic = st.session_state.query
+            query = st.session_state.query
+        st.session_state.messages.append(
+            {"role": "user", "content": f"Topic: {topic} - {query}"}
+        )
+
+        status_message.write("Emedding query...")
+        query_embedding = vector_search.embed_query(topic)
+
+        status_message.write("Searching database for relevant documents...")
+        results = vector_search.simple_vector_search(query_embedding)
+
+        status_message.write("Generating chat completion...")
+        completion_result, usage = vector_search.chat_completion(query, results)
+
+        status.update(label="Done!", state="complete", expanded=False)
+        time.sleep(1)
+        st.session_state.messages.append(
+            {"role": "ai", "content": f"{completion_result} ... cost: ${usage:.6f}"}
+        )
+
+
+def doc_retrieval_pipeline() -> None:
+    with placeholder.status(label="Retrieving documents...", expanded=True) as status:
+        status_message = st.empty()
+
+        query = st.session_state.doc_retrieval
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        status_message.write("Emedding query...")
+        query_embedding = vector_search.embed_query(query)
+
+        status_message.write("Searching database for relevant documents...")
+        results = vector_search.simple_vector_search(query_embedding)
+
+        status.update(label="Done!", state="complete", expanded=False)
+        time.sleep(1)
+        st.session_state.messages.append({"role": "ai", "content": f"{results} "})
+
+
+def doc_retrieval_hybrid_pipeline() -> None:
+    with placeholder.status(label="Retrieving documents...", expanded=True) as status:
+        status_message = st.empty()
+
+        query = st.session_state.hybrid
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        status_message.write("Emedding query...")
+        query_embedding = vector_search.embed_query(query)
+
+        status_message.write("Searching database for relevant documents...")
+        results = vector_search.hybrid_search(query, query_embedding)
+
+        status.update(label="Done!", state="complete", expanded=False)
+        time.sleep(1)
+        st.session_state.messages.append({"role": "ai", "content": f"{results} "})
+
+
+col1, col2 = st.columns(2)
+doc_toggle = col1.toggle("Document retrieval")
+hybrid_toggle = col2.toggle("Hybrid search")
+
+if doc_toggle:
+    st.chat_input(
+        "Retrieve documents", on_submit=doc_retrieval_pipeline, key="doc_retrieval"
+    )
+elif hybrid_toggle:
+    st.chat_input(
+        "Retrieve documents - hybrid",
+        on_submit=doc_retrieval_hybrid_pipeline,
+        key="hybrid",
+    )
+else:
+    st.chat_input("Find answers", on_submit=query_pipeline, key="query")
---
+++ vector_search.py
@@ -0,0 +1,249 @@
+import streamlit as st
+from openai import OpenAI
+from pymongo.mongo_client import MongoClient
+import time
+
+
+# Define GLOBAL VARS
+# Embedding model
+EMBEDDING_MODEL = "text-embedding-3-small"
+EMBEDDING_PRICE_PER_TOKEN = 0.02 / 1_000_000
+
+# MongoDB Vector Search
+VECTOR_NUM_CANDIDATES = 150
+VECTOR_RESULT_LIMIT = 10
+VECTOR_PENALTY = 1
+FULL_TEXT_PENALTY = 1
+
+# Chat completion model
+CHAT_MODEL = "gpt-4o-mini"
+CHAT_INPUT_PRICE = 0.15 / 1_000_000
+CHAT_OUTPUT_PRICE = 0.6 / 1_000_000
+CHAT_TEMPERATURE = 0.2
+CHAT_MAX_OUTPUT_TOKENS = 500
+
+# DB and API connections
+# Connect to OpenAI
+openapi_client = OpenAI(api_key=st.secrets.openai.api_key)
+
+
+# Connect to MongoDB and cache the connection
+@st.cache_resource
+def init_connection():
+    return MongoClient(st.secrets.mongodb.uri)
+
+
+client = init_connection()
+# Connect to DB and collection
+db = client[st.secrets.mongodb.db_name]
+mongo_collection = db.get_collection(st.secrets.mongodb.collection_name)
+
+
+@st.cache_data(ttl="1d", show_spinner=False)
+def embed_query(query: str) -> list[float]:
+    """Takes a query string and returns the vector embedding via openai API,
+    results are cached for 1 day"""
+    embedding_result = openapi_client.embeddings.create(
+        model=EMBEDDING_MODEL, input=[query]
+    )
+    query_embedding = embedding_result.data[0].embedding
+    return query_embedding
+
+
+@st.cache_data(ttl="1d", show_spinner=False)
+def simple_vector_search(query_embedding: list[float]) -> list[dict]:
+    """Takes a query embedding and performs vector search on a MongoDB collection,
+    returns relevant docs. Results are cached for 1 day."""
+    # Define mongodb query pipeline
+    pipeline = [
+        {
+            "$vectorSearch": {
+                "index": "vector_index",
+                "path": "embeddings",
+                "queryVector": query_embedding,
+                "numCandidates": VECTOR_NUM_CANDIDATES,
+                "limit": VECTOR_RESULT_LIMIT,
+            }
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "doc_id": 1,
+                "chunk_id": 1,
+                "text": 1,
+                "score": {"$meta": "vectorSearchScore"},
+            }
+        },
+    ]
+    # run pipeline
+    mongo_result = list(mongo_collection.aggregate(pipeline))
+    return mongo_result
+
+
+def hybrid_search(query: str, query_embedding: list[float]) -> list[dict]:
+    """Takes a query string and query embedding and performs a hybrid search
+    using both vector search and full-text search. Returns a list of relevant docs."""
+    mongo_result = list(
+        mongo_collection.aggregate(
+            [
+                {
+                    "$vectorSearch": {
+                        "index": "vector_index",
+                        "path": "embeddings",
+                        "queryVector": query_embedding,
+                        "numCandidates": VECTOR_NUM_CANDIDATES,
+                        "limit": VECTOR_RESULT_LIMIT,
+                    }
+                },
+                {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
+                {"$unwind": {"path": "$docs", "includeArrayIndex": "rank"}},
+                {
+                    "$addFields": {
+                        "vs_score": {
+                            "$divide": [1.0, {"$add": ["$rank", VECTOR_PENALTY, 1]}]
+                        }
+                    }
+                },
+                # {"$project": {"vs_score": 1, "_id": "$docs._id", "text": "$docs.text"}},
+                {
+                    "$project": {
+                        "vs_score": 1,
+                        "doc_id": "$docs.doc_id",
+                        "chunk_id": "$docs.chunk_id",
+                        "text": "$docs.text",
+                    }
+                },
+                {
+                    "$unionWith": {
+                        "coll": st.secrets.mongodb.collection_name,
+                        "pipeline": [
+                            {"$search": {"phrase": {"query": query, "path": "text"}}},
+                            {"$limit": 20},
+                            {"$group": {"_id": None, "docs": {"$push": "$$ROOT"}}},
+                            {"$unwind": {"path": "$docs", "includeArrayIndex": "rank"}},
+                            {
+                                "$addFields": {
+                                    "fts_score": {
+                                        "$divide": [
+                                            1.0,
+                                            {"$add": ["$rank", FULL_TEXT_PENALTY, 1]},
+                                        ]
+                                    }
+                                }
+                            },
+                            {
+                                "$project": {
+                                    "fts_score": 1,
+                                    # "_id": "$docs._id",
+                                    "doc_id": "$docs.doc_id",
+                                    "chunk_id": "$docs.chunk_id",
+                                    "text": "$docs.text",
+                                }
+                            },
+                        ],
+                    }
+                },
+                {
+                    "$group": {
+                        "_id": {"doc_id": "$doc_id", "chunk_id": "$chunk_id"},
+                        "text": {"$first": "$text"},
+                        "vs_score": {"$max": "$vs_score"},
+                        "fts_score": {"$max": "$fts_score"},
+                    }
+                },
+                {
+                    "$project": {
+                        "_id": 1,
+                        "text": 1,
+                        "chunk_id": 1,
+                        "doc_id": 1,
+                        "vs_score": {"$ifNull": ["$vs_score", 0]},
+                        "fts_score": {"$ifNull": ["$fts_score", 0]},
+                    }
+                },
+                {
+                    "$project": {
+                        "score": {"$add": ["$fts_score", "$vs_score"]},
+                        "_id": 1,
+                        "text": 1,
+                        "chunk_id": 1,
+                        "doc_id": 1,
+                        "vs_score": 1,
+                        "fts_score": 1,
+                    }
+                },
+                {"$sort": {"score": -1}},
+                {"$limit": 10},
+            ]
+        )
+    )
+    return mongo_result
+
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "make_bar_chart",
+            "description": """Generates a bar chart from a list of x and y values. 
+            The x values are the 'vectorSearchscores' as floats and the y values are the 
+            document IDs which should be strings - both from the returned 'docs'.
+            Call this function when a user asks for a bar chart of the doc ids and vector search scores.""",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "vector_search_score": {
+                        "type": "array",
+                        "items": {"type": "number"},
+                        "description": "The vector search scores for each document.",
+                    },
+                    "doc_ids": {
+                        "type": "array",
+                        "items": {"type": "string"},
+                        "description": "The document IDs for each document.",
+                    },
+                },
+                "required": ["vector_search_score", "doc_ids"],
+                "additionalProperties": False,
+            },
+        },
+    }
+]
+
+
+@st.cache_data(ttl="1d", show_spinner=False)
+def chat_completion(query: str, docs: list[dict]) -> str:
+    """Takes a query string, and list of retrieved docs
+    and returns a chat completion. Results are cached for 1 day."""
+    completion_result = openapi_client.chat.completions.create(
+        model=CHAT_MODEL,
+        temperature=CHAT_TEMPERATURE,
+        max_tokens=CHAT_MAX_OUTPUT_TOKENS,
+        messages=[
+            {
+                "role": "system",
+                "content": """You are a helpful research assistant. 
+             Please only base your answers on the list of documents provided.
+             Ignore /n characters as they may represent newlines. 
+             Do not use external sources. Note the query will follow the word query and the documents will follow the word documents.
+             Please cite the 'doc_id' and 'chunk_id' of the document you are referencing for each part of your answer.""",
+            },
+            {
+                "role": "user",
+                "content": f"Based on the following documents: {docs}, answer the following query: {query}",
+            },
+        ],
+        tools=tools,
+        tool_choice="auto",
+    )
+
+    usage = (
+        completion_result.usage.prompt_tokens * CHAT_INPUT_PRICE
+        + completion_result.usage.completion_tokens * CHAT_OUTPUT_PRICE
+    )
+
+    if completion_result.choices[0].finish_reason == "tool_calls":
+        st.session_state.tool_call = completion_result.choices[0].message.tool_calls[0]
+        return completion_result.choices[0].message.tool_calls[0], usage
+    else:
+        return completion_result.choices[0].message.content, usage
